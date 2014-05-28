1ef95f0dc5d005a82c3784400ceef0ab0203163f
ggaaooppeenngg <471160161@qq.com>
2014-05-28 21:09:28 +0800

    use docker as sandboxie

--- src/OJ/app/check/check.go
+++ src/OJ/app/check/check.go
@@ -19,6 +19,37 @@ func init() {
 	engine = models.Engine()
 }
 
+//use command sed repleace SRCFILE to real source file
+func genDocFile(path string) error {
+	out, err := util.Run("sed", "s/SRCFILE/"+path+"/g", "Seedfile")
+	if err != nil {
+		return err
+	}
+	_, err = util.WriteFile("Dockerfile", out)
+	if err != nil {
+		return err
+	}
+}
+
+//clean the container after running
+func removeContainer() {
+	util.Run("docker", "rm", "check")
+}
+
+//user generated dockfile to build and run the test
+func test(path string) {
+	defer removeContainer()
+	genDocFile(path)
+	_, err := util.Run("docker", "build", "-t", imageName, ".")
+	if err != nil {
+		fmt.Println(err)
+	}
+	out, err := util.Run("docker", "run", "--name=check", imageName, "go", "run", "/home/main.go")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
 func Do() {
 	var sources []models.Source
 	err := engine.Where("status = ?", models.UnHandled).Find(&sources)
@@ -27,12 +58,7 @@ func Do() {
 		fmt.Println(err)
 	}
 	for _, v := range sources {
-		//util.Run("docker", "build", "-t", imageName, ".")
-		//util.Run("docker", imageName, "go", "run", "main.go")
-		out, err := util.Run("go", "run", v.Path)
-		if err != nil {
-			fmt.Println(err)
-		}
+		out, _ := util.Run("go", "run", v.Path)
 		if string(out) != "Hello World\n" {
 			v.Status = models.WrongAnswer
 			engine.Id(v.Id).Cols("status").Update(&v)
