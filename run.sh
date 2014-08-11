2f42a0a330b8bd3eb0981f52f456662f73ca759f
ggaaooppeenngg <471160171@qq.com>
2014-08-12 01:21:18 +0800

    deploy modify

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-go build -o judge/judge github.com/ggaaooppeenngg/OJ/judge
+go build -o judge/judge github.com/ggaaooppeenngg/OJ/judge/main.go
 judge/judge &
 echo "begin judge loop"
 revel run github.com/ggaaooppeenngg/OJ
